9b437db94b3147c8f2df553c9f9cdfccf35be078
Hans1214 <rhanscedric@gmai.com>
2024-12-13 21:24:48 +0800

    first commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,6 @@
+const div = document.querySelector("div")
+const spanHi = document.ATTRIBUTE_NODE.querySelector("#hi")
+const spanHi = document.ATTRIBUTE_NODE.querySelector("#bye")
+
+spanBye.remove()
+div.append(spanBye)
